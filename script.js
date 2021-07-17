fb784d92e2e99d298629fad4085c015396579df9
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2021-07-17 22:29:24 +0300

    This will add the start of the calculator part

--- script.js
+++ script.js
@@ -4,8 +4,26 @@ const female = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 
 //Beginning of the input section
 
-let yearInput = document.querySelector("the input class").value;
-let monthInput =
+let dayInput = document.querySelector(".day").value;
+let monthInput =document.querySelector(".month").addEventListener('click', function (){
+    siku =//.getDate();
+    //.getMonth() + 1;
+    //.getFullYear();
+    let clientDate = new Date(tarehe);
+    let clientDay = date.toLocaleString('en-us', {weekday: 'long'});
+    alert(clientDay);
+});
+//extracting day
+
+
+
+
+let yearInput = document.querySelector(".year").value;
+
+let tarehe = `${monthInput},${dayInput},${yearInput}`;
+
+let clientDate = new Date(tarehe);//add Number
+alert(monthInput);
 
 //Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
 //Implementation two starts here
