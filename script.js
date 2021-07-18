dd70fe961a1172ca99c510c1033868c420968810
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2021-07-18 13:23:32 +0300

    This will add functionality to the calender

--- script.js
+++ script.js
@@ -1,35 +1,7 @@
-const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const male = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-const female = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
-
-//Beginning of the input section
-
-let dayInput = document.querySelector(".day").value;
-let monthInput =document.querySelector(".submitBtn").addEventListener('click', function (){
-    const siku =document.querySelector(".month").getDate();
-    const mwezi =document.querySelector(".month").getMonth() + 1;
-    const mwaka = document.querySelector(".month").getFullYear();
-    // let compiledDate=`${siku}/${mwezi}/${mwaka}`;
-    let clientDate = new Date(Number(`${siku}/${mwezi}/${mwaka}`));
-    let clientDay = clientDate.toLocaleString('en-us', {weekday: 'long'});
-    console.log(clientDay);
-});
-//extracting day
-
-
-
-
-let yearInput = document.querySelector(".year").value;
-
-let tarehe = `${monthInput},${dayInput},${yearInput}`;
-
-let clientDate = new Date(tarehe);//add Number
-alert(monthInput);
-
-//Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
-//Implementation two starts here
-
-const ndume = {
+// const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+// const male = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+// const female = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+const male = {
     Sunday: "Kwasi",
     Monday: "Kwadwo",
     Tuesday: "Kwabena",
@@ -39,7 +11,7 @@ const ndume = {
     Saturday: "Kwame"
 }
 
-const jike = {
+const female = {
     Sunday: "Akosua",
     Monday: "Adwoa",
     Tuesday: "Abenaa",
@@ -48,3 +20,112 @@ const jike = {
     Friday: "Afua",
     Saturday: "Ama"
 }
+const months = {
+    1 : "January",
+    2 : "February",
+    3 : "March",
+    4 : "April",
+    5 : "May",
+    6 : "June",
+    7 : "July",
+    8 : "August",
+    9 : "September",
+    10 : "October",
+    11 : "November",
+    12 : "December"
+}
+//Beginning of the input section
+
+
+document.querySelector('.inputDate').addEventListener("change", function(){
+    let dayInput = this.value;
+    let tarehe= new Date(dayInput);
+    let timeInput =function(){
+        let siku =tarehe.getDate();
+        let mwezi =tarehe.getMonth()+1;
+        let mwaka = tarehe.getFullYear();
+        alert(`${siku}/${mwezi}/${mwaka}`);
+        alert(`The date is: ${siku}`)
+        let clientDay = tarehe.toLocaleString('en-us', {weekday: "long"});
+        alert((clientDay));
+        let i = Number(mwezi);// passing the mwezi/month value to variable i
+        let monthName = months[i]
+        console.log(monthName);//accessing the months object
+        let akanMale= male[clientDay];
+        let akanFemale= female[clientDay];
+        console.log(akanMale);
+        console .log(akanFemale);
+
+    };
+    timeInput();
+})
+
+sex
+//getting client gender
+document.querySelector('.maleGender').addEventListener('click', function(){
+
+})
+
+// let timeInput =document.querySelector(".submitBtn").addEventListener('click', function (){
+
+
+
+
+// let dayInput = document.getElementsByClassName('.inputDate').value;
+// console.log(typeof(dayInput));
+// let chill = new Date(dayInput);
+// console.log(chill);
+//extracting day
+
+
+
+
+// let yearInput = document.querySelector(".year");
+
+// let tarehe = `${monthInput},${dayInput},${yearInput}`;
+
+// let clientDate = new Date(tarehe);//add Number
+// alert(monthInput);
+
+//Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
+//Implementation two starts here
+
+// const months = {
+//     1 : "January",
+//     2 : "February",
+//     3 : "March",
+//     4 : "April",
+//     5 : "May",
+//     6 : "June",
+//     7 : "July",
+//     8 : "August",
+//     9 : "September",
+//     10 : "October",
+//     11 : "November",
+//     12 : "December"
+// }
+
+// const male = {
+//     Sunday: "Kwasi",
+//     Monday: "Kwadwo",
+//     Tuesday: "Kwabena",
+//     Wednesday:"Kwaku",
+//     Thursday:  "Yaw",
+//     Friday: "Kofi",
+//     Saturday: "Kwame"
+// }
+
+// const female = {
+//     Sunday: "Akosua",
+//     Monday: "Adwoa",
+//     Tuesday: "Abenaa",
+//     Wednesday: "Akua",
+//     Thursday:  "Yaa",
+//     Friday: "Afua",
+//     Saturday: "Ama"
+// }
+
+//name input
+const identification=document.querySelector('.jina').textContent;
+
+
